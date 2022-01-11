8e2be24e3e9771557bbe0a20b6aac0773acedefe
Moises Bruno Gregorio <brunomoises@gmail.com>
2022-01-11 19:53:16 -0300

    Contracts list for authenticated user

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { Op } = require("sequelize");
 const {sequelize} = require('./model')
 const {getProfile} = require('./middleware/getProfile')
 const app = express();
@@ -17,6 +18,27 @@ app.get('/contracts/:id',getProfile ,async (req, res) =>{
     const ContractorId = req.profile.get('id')
 
     const contract = await Contract.findOne({where: {id, ContractorId}})
+    if(!contract) return res.status(404).end()
+    res.json(contract)
+})
+
+/**
+ * Returns a list of contracts belonging to a user
+ * @returns Contract[]
+ */
+app.get('/contracts', getProfile, async (req, res) =>{
+    const {Contract} = req.app.get('models')
+    const {id} = req.params
+    const profile_id = req.profile.get('id')
+
+    const contract = await Contract.findAll({
+        where:  {
+            Status: {
+                [Op.ne]: 'terminated'
+            },
+            [Op.or]: [{ContractorId : profile_id}, {ClientId : profile_id}]
+        }
+    })
     if(!contract) return res.status(404).end()
     res.json(contract)
 })
